33b16a5a6307842d560b9873c7a1867e32b85dbf
softechsuperstar <perv.expert610@outlook.com>
2021-10-14 11:22:12 +0200

    complete github api

---
+++ src/pages/GithubSearch.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const GithubSearch = () => {
+  const [username, setUsername] = useState(null)
+  const getUser = (e) => {
+    e.preventDefault();
+    console.log(username)
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+      })
+  }
+  const getRepos = (e) => {
+    e.preventDefault();
+    console.log(username)
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+      })
+  }
+  return (
+    <div>
+      <h3>GithubSearch</h3>
+        <div>
+          <input type="text" onInput={(e) => setUsername(e.target.value)}/>
+        </div>
+        <div>
+          <button onClick={getUser}>Get user Info</button>
+        </div>
+        <div>
+          <button onClick={getRepos}>Get user Info</button>
+        </div>
+    </div>
+  )
+}
+
+export default GithubSearch;
