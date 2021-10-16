a4ecbbb239724eda99f7b10247348f55ba1f8026
softechsuperstar <perv.expert610@outlook.com>
2021-10-16 17:44:53 +0200

    delete server

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const coinRoute = require('./coin.route');
-
-router.use('/coin', coinRoute);
-
-module.exports = router;
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,28 +1,12 @@
 import React, { useEffect } from 'react';
-import CoinpaprikaAPI from '@coinpaprika/api-nodejs-client';
-const client = new CoinpaprikaAPI();
- 
+import axios from 'axios'; 
 // 
 // client.getGlobal().then(console.log).catch(console.error);
 const Work = () => {
   useEffect(() => {
-    // Get global information
-    client.getGlobal()
-      .then((res) => {
-        console.log("getGlobal", res);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-
-
-    client.getTicker()
-      .then((res) => {
-        console.log("getTicker", res);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    axios.get('https://www.cryptingup.com/api/markets').then((res) => {
+      console.log(res)
+    })
 
 
   }); // eslint-disable-line react-hooks/exhaustive-deps
